b04ecf1c276c2b496b71acb0effba41b04372993
Jose Luis <carrerasoto53@gmail.com>
2025-12-07 18:03:33 +0000

    Resolver conflicto en cors.php - usar versión remota

--- config/cors.php
+++ config/cors.php
@@ -6,16 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:8100', 
-        'http://127.0.0.1:8100', 
-        'http://192.168.1.6:8100', 
-        'http://192.168.1.15:8100',
-        'http://localhost:4200',
-        'https://panel.gturnos.tech',
-        'https://gturnos.tech',
-        'https://sucursal.gturnos.tech'
-    ],
+    'allowed_origins' => ['https://panel.gturnos.tech','https://gturnos.tech','https://sucursal.gturnos.tech' ],
 
     'allowed_origins_patterns' => [],
 
